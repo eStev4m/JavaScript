55aa39881d9539b6fbd04552a307db17b643809f
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2022-09-30 11:49:51 -0300

    ex12maisumpouco

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#ffe955'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#a7bbc9'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#5b4265'
+    }
+}
