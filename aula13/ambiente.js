e5280674be5cc67844c557e801459bab30208c26
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2022-10-03 12:36:29 -0300

    aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while (c <= 10) {
+    console.log(`Números ${c}.`)
+    c++
+}
+
+var c = 1
+do {
+    console.log(`Números ${c}.`)
+    c++
+}while (c <= 10)
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Inicio de contagem')
+for (var c = 1; c <= 10; c++) {
+    console.log(`Número ${c}`)
+}
+console.log('FIM!')
