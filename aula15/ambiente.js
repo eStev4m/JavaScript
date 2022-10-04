dcc674dbc2fc232c0b9d34ffd7e99ec44584cfdf
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2022-10-04 17:38:30 -0300

    aula15 terminada

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,5 +1,19 @@
-let num = [3, 5, 1, 8]
-num[4] = 10
-num.push(125)
-console.log(`${num}`)
-//time video: 14:05
+let num = [8, 5, 6, 3]
+num[4] = 9
+num.push(1)
+
+console.log(`Números no meu vetor: ${num.sort()}`)
+console.log(`Quantidade de posições no meu vetor: ${num.length}`)
+console.log(`Qual a posição do numero 9: ${num.indexOf(9)}`)
+/*
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+*/
+
+
+//Forma atual
+
+for (let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
