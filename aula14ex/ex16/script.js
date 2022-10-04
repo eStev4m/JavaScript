b3bc416e12fbb4b96568b73af9a79b0b89019fb0
Estevam Jannuzzi <85847449+eStev4m@users.noreply.github.com>
2022-10-04 11:39:46 -0300

    aulas 04-10-2022

--- aula14ex/ex16/script.js
+++ aula14ex/ex16/script.js
@@ -1,7 +1,30 @@
 function contar() {
-    var ini = document.getElementById('txtini')
-    var fim = document.getElementById('txtfim')
-    var pas = document.getElementById('txtpas')
-    var res = document.querySelector('div#res')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.querySelector('div#res')
 
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{27A1} `
+            }
+        } else {
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{2B05} `
+            }
+        }
+        res.innerHTML += `\u{2714}`
     }
+}
